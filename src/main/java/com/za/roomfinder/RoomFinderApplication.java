9c70c4a92951a949f2589d027acd5325077e02d1
mmeiring021 <mmeiring021@student.wethinkcode.co.za>
2023-02-16 16:00:20 +0200

    Start of my room finder application

---
+++ src/main/java/com/za/roomfinder/RoomFinderApplication.java
@@ -0,0 +1,13 @@
+package com.za.roomfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/za/roomfinder/RoomFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.za.roomfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
