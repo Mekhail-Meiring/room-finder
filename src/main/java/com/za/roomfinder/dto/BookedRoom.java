e2405757776cebf7b6e65dbd1bb0d86031b8cf16
mmeiring021 <mmeiring021@student.wethinkcode.co.za>
2023-02-18 19:44:29 +0200

    Added from client field

--- src/main/java/com/za/roomfinder/dto/BookedRoom.java
+++ src/main/java/com/za/roomfinder/dto/BookedRoom.java
@@ -3,7 +3,8 @@ package com.za.roomfinder.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record BookedRoom(
-    @JsonProperty("client_id") String clientId,
+    @JsonProperty("from_client_id") int fromClientId,
+    @JsonProperty("booking_id") int bookingId,
     @JsonProperty("start_date") String startDate,
     @JsonProperty("end_date") String endDate,
     @JsonProperty("price") double price
