4d887ba21ae48ebeab4e435168ea04442b08b267
mmeiring021 <mmeiring021@student.wethinkcode.co.za>
2023-02-18 19:44:54 +0200

    Changed field type to int

--- src/main/java/com/za/roomfinder/dto/BookingRequest.java
+++ src/main/java/com/za/roomfinder/dto/BookingRequest.java
@@ -3,7 +3,7 @@ package com.za.roomfinder.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record BookingRequest(
-    @JsonProperty("client_id") String clientId,
+    @JsonProperty("client_id") int clientId,
     @JsonProperty("start_date") String startDate,
     @JsonProperty("end_date") String endDate
 ) {
