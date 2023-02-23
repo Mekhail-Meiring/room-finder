610d8cd00026d9260d766149f6b27d28589f4036
mmeiring021 <mmeiring021@student.wethinkcode.co.za>
2023-02-23 10:01:33 +0200

    updated the roomfinder service to use its datasource. Also added a test to see that it does

--- src/main/java/com/za/roomfinder/service/RoomFinderService.java
+++ src/main/java/com/za/roomfinder/service/RoomFinderService.java
@@ -1,19 +1,59 @@
 package com.za.roomfinder.service;
 
-import com.za.roomfinder.datasource.RoomFinderDataSource;
-import com.za.roomfinder.dto.Client;
+import com.za.roomfinder.service.datasource.RoomFinderDataSource;
+import com.za.roomfinder.service.datasource.dto.BookedRoom;
+import com.za.roomfinder.service.datasource.dto.BookingRequest;
+import com.za.roomfinder.service.datasource.dto.Client;
+import com.za.roomfinder.service.datasource.dto.RoomPrice;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoomFinderService {
 
-    private final RoomFinderDataSource roomFinderDataSource;
+    private final RoomFinderDataSource dataSource;
 
-    public RoomFinderService(RoomFinderDataSource roomFinderDataSource) {
-        this.roomFinderDataSource = roomFinderDataSource;
+    public RoomFinderService(RoomFinderDataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
-    public Client registerClient(Client client) {
-        return roomFinderDataSource.registerClient(client);
+
+    public void registerClient(Client client) {
+         dataSource.registerClient(client);
     }
+
+
+    public Client clientLogin(int clientId){
+        return dataSource.clientLogin(clientId);
+    }
+
+
+    public RoomPrice bookRoom(BookingRequest bookingRequest) {
+        return dataSource.bookRoom(bookingRequest);
+    }
+
+    public void payForBooking(Double price, BookingRequest bookingRequest){
+        dataSource.payForBooking(price, bookingRequest);
+    }
+
+
+    public RoomPrice rescheduleBooking(int bookingId, BookingRequest bookingRequest) {
+        return dataSource.rescheduleBooking(bookingId, bookingRequest);
+    }
+
+    public void payForReschedule(int bookingId, double price, BookingRequest bookingRequest){
+        dataSource.payForReschedule(bookingId, price, bookingRequest);
+    }
+
+
+    public Double cancelBooking(int bookingId, int clientId) {
+        return dataSource.cancelBooking(bookingId, clientId);
+    }
+
+
+    public List<BookedRoom> getBookedRooms() {
+        return dataSource.getBookedRooms();
+    }
+
 }
---
+++ src/test/java/com/za/roomfinder/service/RoomFinderServiceTest.java
@@ -0,0 +1,102 @@
+package com.za.roomfinder.service;
+
+import com.za.roomfinder.service.datasource.RoomFinderDataSource;
+
+import com.za.roomfinder.service.datasource.dto.BookingRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class RoomFinderServiceTest {
+
+    RoomFinderDataSource dataSource = Mockito.mock(RoomFinderDataSource.class);
+
+    private final RoomFinderService service = new RoomFinderService(dataSource);
+
+
+    @Test
+    @DisplayName("Should call its datasource to register a client")
+    public void test1(){
+
+        // When:
+        service.registerClient(null);
+
+        // Then:
+        Mockito.verify(dataSource, Mockito.times(1)).registerClient(null);
+    }
+
+
+    @Test
+    @DisplayName("Should call its datasource to get all bookings made")
+    public void test2(){
+
+        // When:
+        service.getBookedRooms();
+
+        // Then:
+        Mockito.verify(dataSource, Mockito.times(1)).getBookedRooms();
+    }
+
+
+    @Test
+    @DisplayName("Should call its datasource to reschedule a booking")
+    public void test3(){
+
+        // When:
+        service.rescheduleBooking(1, null);
+
+        // Then:
+        Mockito.verify(dataSource, Mockito.times(1)).rescheduleBooking(1, null);
+    }
+
+
+    @Test
+    @DisplayName("Should call its datasource to cancel a booking")
+    public void test4(){
+
+        // When:
+        service.cancelBooking(1,1);
+
+        // Then:
+        Mockito.verify(dataSource, Mockito.times(1)).cancelBooking(1,1);
+    }
+
+
+    @Test
+    @DisplayName("Should call its datasource to book a room")
+    public void test5(){
+
+        // When:
+        service.bookRoom(null);
+
+        // Then:
+        Mockito.verify(dataSource, Mockito.times(1)).bookRoom(null);
+    }
+
+
+    @Test
+    @DisplayName("Should call its datasource when a client logs in")
+    public void test6(){
+
+        // When:
+        service.clientLogin(1);
+
+        // Then:
+        Mockito.verify(dataSource, Mockito.times(1)).clientLogin(1);
+    }
+
+    @Test
+    @DisplayName("Should call its datasource when a client confirms a booking")
+    public void test7(){
+
+        // Given
+        BookingRequest bookingRequest = new BookingRequest(1, "2023-02-23");
+
+        // When:
+        service.payForBooking(0.0, bookingRequest);
+
+        // Then:
+        Mockito.verify(dataSource, Mockito.times(1)).payForBooking(0.0, bookingRequest);
+
+    }
+}
